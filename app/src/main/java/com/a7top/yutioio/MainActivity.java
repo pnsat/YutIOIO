be5e2ad001e9ee612d2ffb92a284b71f8e631829
masterUng <ubc1515@hotmail.com>
2017-04-07 11:52:33 +0700

    Connect Borad OK

--- app/src/main/java/com/a7top/yutioio/MainActivity.java
+++ app/src/main/java/com/a7top/yutioio/MainActivity.java
@@ -2,15 +2,22 @@ package com.a7top.yutioio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.IOIOLooper;
 import ioio.lib.util.android.IOIOActivity;
 
 public class MainActivity extends IOIOActivity implements View.OnClickListener {
 
     private Button button;
+    private String tag = "7apiv1";
+    private boolean aBoolean = false;
+
 
 
     @Override
@@ -32,9 +39,45 @@ public class MainActivity extends IOIOActivity implements View.OnClickListener {
 
     class Looper extends BaseIOIOLooper {
 
+        @Override
+        protected void setup() throws ConnectionLostException, InterruptedException {
+            //super.setup();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),"Connected IOIO OK",
+                            Toast.LENGTH_LONG).show();
+                    button.setText("Connecter OK");
+
+                }
+            });
+
+        }  // setup
+
+        @Override
+        public void loop() throws ConnectionLostException, InterruptedException {
+           // super.loop();
+
+            try {
+                Thread.sleep(100);
+
+
+            } catch (Exception e) {
+                Log.d(tag, "e loop ==>" + e.toString());
+
+
+            }
+
+        }
 
 
     }  // Looper Class
+
+    protected IOIOLooper createIOIOLooper() {
+
+        return new Looper();
+    }
 
 
 
